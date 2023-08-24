dd9f4f3809ba688960512c18656f43f44963491e
Samuel33d <sbenedetti89@gmail.com>
2023-08-24 01:36:22 -0400

    mi primer commit

---
+++ js/index.js
@@ -0,0 +1,31 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.navbar',{
+    duration: 800,
+    origin:"top",
+    distance: '100px'
+})
+
+sr.reveal('.header__title',{
+    duration: 2000,
+    origin:"bottom",
+    distance: '100px'
+})
+
+sr.reveal('.section__card',{
+    duration: 2000,
+    origin:"bottom",
+    distance: '100px'
+})
+
+sr.reveal('.proyect__card',{
+    duration: 2000,
+    origin:"bottom",
+    distance: '100px'
+})
+
+sr.reveal('.contact_icon',{
+    duration: 2000,
+    origin:"right",
+    distance: '100px'
+})
