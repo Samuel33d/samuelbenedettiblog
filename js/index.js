bbf2cfb09087e9d6f7a61fc187873885b4c3981d
Samuel33d <sbenedetti89@gmail.com>
2023-09-14 05:22:52 -0400

    fix responsive

--- js/index.js
+++ js/index.js
@@ -1,31 +1,56 @@
 window.sr = ScrollReveal();
 
-sr.reveal('.navbar',{
+sr.reveal('.navbar', {
     duration: 800,
-    origin:"top",
+    origin: "top",
     distance: '100px'
 })
 
-sr.reveal('.header__title',{
+sr.reveal('.header__title', {
     duration: 2000,
-    origin:"bottom",
+    origin: "bottom",
     distance: '100px'
 })
 
-sr.reveal('.section__card',{
+sr.reveal('.section__card', {
     duration: 2000,
-    origin:"bottom",
+    origin: "bottom",
     distance: '100px'
 })
 
-sr.reveal('.proyect__card',{
+sr.reveal('.proyect__card', {
     duration: 2000,
-    origin:"bottom",
+    origin: "bottom",
     distance: '100px'
 })
 
-sr.reveal('.contact_icon',{
+sr.reveal('.contact_icon', {
     duration: 2000,
-    origin:"right",
+    origin: "bottom",
     distance: '100px'
+})
+
+
+const iconNav = document.querySelector(".navbar__icon")
+const navMenu = document.querySelector(".navbar__menu")
+const closeMenu = document.querySelector(".closeMenu")
+const handleMenu = document.querySelector(".navbar__menu-a")
+
+iconNav.addEventListener("click", function () {
+    navMenu.classList.add("show__menu")
+
+    closeMenu.addEventListener("click", function () {
+        navMenu.classList.remove("show__menu")
+    })
+
+    handleMenu.addEventListener("click", function () {
+        navMenu.classList.remove("show__menu")
+    })
+})
+
+const navbar = document.querySelector(".navbar")
+
+window.addEventListener("scroll", function () {
+    navbar.classList.toggle("navScroll", window.scrollY > 0)
+
 })
